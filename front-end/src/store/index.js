f174d663eb0545ee3017ea4926e657e6b802863c
김규리 <qri1114@gmail.com>
2022-11-15 17:56:11 +0900

    login view 구현

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,16 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+const API_URL = 'http://127.0.0.1:8000'
+
 export default new Vuex.Store({
   state: {
+    token: null,
   },
   getters: {
   },
   mutations: {
+    SAVE_TOKEN(state, token){
+      state.token = token;
+      console.log('저장됨ㅋ');
+    }
   },
   actions: {
+    signUp(context, payload){
+      axios({
+        method: 'POST',
+        url: `${API_URL}/accounts/signup/`,
+        data:{
+          username: payload.username,
+          password1: payload.password1,
+          password2: payload.password2,
+        }
+      })
+        .then((response)=>{
+          context.commit('SAVE_TOKEN', response.data.key);
+        })
+        .catch((error)=>{
+          console.log(error);
+        })
+    }
   },
   modules: {
   }
---
+++ front-end/src/views/SignUpView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>회원가입</h1>
+    <form @submit.prevent="signUp">
+      <label for="username">username: </label>
+      <input type="text" id="username" v-model="username">
+      
+      <label for="password1">password: </label>
+      <input type="password" id="password1" v-model="password1">
+      
+      <label for="password2">password 확인: </label>
+      <input type="password" id="password2" v-model="password2">
+
+      <input type="submit" value="회원가입">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUpView',
+  data(){
+    return{
+      username: null,
+      password1: null,
+      password2: null,
+    }
+  },
+  methods: {
+    signUp(){
+      const payload = {
+        username: this.username,
+        password1: this.password1,
+        password2: this.password2,
+      }
+      this.$store.dispatch('signUp', payload)
+
+      this.username = null
+      this.password1 = null
+      this.password2 = null
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
