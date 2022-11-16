0c40d10174675a35edf0a534c840d6e98f1e4274
shpark0913 <shpark097754@gmail.com>
2022-11-16 17:47:47 +0900

    1116_Movie Model 구현

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Movie
+from .serializers import MovieListSerializer
+from rest_framework.response import Response 
 # Create your views here.
+
+def movie_list_json(request):
+    movies = Movie.objects.all()
+    serializer = MovieListSerializer(movies, many=True)
+    return Response(serializer.data)
