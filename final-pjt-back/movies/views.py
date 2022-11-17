316253014158b97d03f8bc6dd070eaa013390e74
shpark0913 <shpark097754@gmail.com>
2022-11-17 15:46:26 +0900

    1117_ManyToMany 구현

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from django.http import HttpResponse
 import requests
 
-from .models import Movie
+from .models import Movie, Genre
 from .serializers import MovieListSerializer, MovieDetailSerializer
 # Create your views here.
 
@@ -29,7 +29,6 @@ def get_movie_datas(request):
         request_url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&language=ko-KR&page={i}"
         movies = requests.get(request_url).json()
         for movie in movies['results']:
-            print(movie)
             movie_instance = Movie()
             movie_instance.title  = movie['title']
             movie_instance.movieid  = movie['id']
@@ -38,8 +37,21 @@ def get_movie_datas(request):
             movie_instance.vote_average = movie['vote_average']
             movie_instance.overview = movie['overview']
             movie_instance.poster_path = movie['poster_path']
-            movie_instance.genres = movie.get('genre_ids')   
+            # movie_instance.genres = movie.get('genre_ids')   
             if movie_instance.overview and movie_instance.release_date and movie_instance.poster_path and movie_instance.backdrop_path:
                 movie_instance.save()
+                for genre in movie.get('genre_ids'):
+                    movie_instance.genres.add(genre)
+    return HttpResponse()
 
+
+def get_genre(request):
+    request_url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={API_KEY}&language=ko-KR"
+    genres = requests.get(request_url).json()
+    for genre in genres['genres']:
+        g = Genre()
+        g.genreid = genre['id']
+        g.name = genre['name']
+        if g.genreid and g.name:
+            g.save()
     return HttpResponse()
