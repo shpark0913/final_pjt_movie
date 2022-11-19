19eda49a7fc4c8dd6d25a756b6960232732582f6
shpark0913 <shpark097754@gmail.com>
2022-11-19 18:06:31 +0900

    1119_액션영화 10개 추천, 로맨스영화  10개 추천 알고리즘

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -4,9 +4,15 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
+    path('action10/', views.action10, name='action10'),
+    path('romance10/', views.romance10, name='romance10'),
+    
+    
+    path('get_movie_datas/', views.get_movie_datas, name='get_movie_datas'),
+    path('get_genre/', views.get_genre, name='get_genre'),
+    
+    
     path('<int:movieid>/', views.movie_detail, name='movie_detail'),
     path('<int:movieid>/review/', views.review, name='review'),
     path('review/<int:review_pk>/', views.review_UD, name='review_UD'),
-    path('get_movie_datas/', views.get_movie_datas, name='get_movie_datas'),
-    path('get_genre/', views.get_genre, name='get_genre'),
 ]
