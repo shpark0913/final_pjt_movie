3fee6556f69f0b6e8346dbee1a8abd90f886ff5e
shpark0913 <shpark097754@gmail.com>
2022-11-21 12:27:51 +0900

    1121_영화 detail에서 장르를 pk가 아닌 이름으로 조회하기 기능 구현

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
-from .models import Movie, Review
+from .models import Movie, Review, Genre
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = '__all__'
+        read_only_fields = ('genre',)
+
 
 class MovieListSerializer(serializers.ModelSerializer):
 
@@ -7,15 +15,18 @@ class MovieListSerializer(serializers.ModelSerializer):
         model = Movie
         fields = '__all__'
 
-class ReviewListSerializer(serializers.ModelSerializer):
 
+class ReviewListSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Review
         fields = '__all__'
         read_only_fields = ('movie', 'user',)
-        
+
+
 class MovieDetailSerializer(serializers.ModelSerializer):
     review_set = ReviewListSerializer(many=True, read_only=True)
+    genres = GenreSerializer(many=True, read_only=True)
     class Meta:
         model = Movie
         fields = '__all__'
