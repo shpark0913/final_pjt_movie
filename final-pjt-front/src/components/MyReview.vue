3602c09042bb3e1926cdea427d2f21fd9719cbab
김규리 <qri1114@gmail.com>
2022-11-22 11:41:16 +0900

    1122_mypage review섹션

---
+++ final-pjt-front/src/components/MyReview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ review.content }}</p>
+    <p>{{ review.vote_average }}</p>
+    <!-- <img v-if="imgSrc" :src="imgSrc" alt=""> -->
+    <hr>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios';
+// const API_KEY = '2808f49cb7a157269d3c55874b8d52e5';
+
+export default {
+  name: 'MyReview',
+  props: {
+    review: Object,
+  },
+}
+</script>
+
+<style>
+
+</style>
